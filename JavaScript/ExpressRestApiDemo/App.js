72162a2d3bb7296493acd050eb62320fb3709003
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-12 11:53:12 +0200

    Lisätty kaksi uutta reittiä Express.js-sovellukseen.

--- JavaScript/ExpressRestApiDemo/App.js
+++ JavaScript/ExpressRestApiDemo/App.js
@@ -7,11 +7,32 @@ app.get('/', (req, res) => {
     const olio = {
         nimi: "Matti Möttönen",
         puhelin: "050 111 2222"
-    }
+    };
 
     res.json(olio);
-})
+});
+
+app.get('/toka', (req, res) => {
+  
+    const olio = {
+        nimi: "Teppo Testaaja",
+        puhelin: "050 9876 543"
+    };
+
+    res.json(olio);
+});
+
+app.post('/', (req, res) => {
+  
+    const status = {
+        success: true,
+        error: null
+    };
+
+    res.json(status);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
