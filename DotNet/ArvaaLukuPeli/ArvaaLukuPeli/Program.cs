60083785ba402aa24a92107fdfb9d1fb73a70ee7
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-24 13:15:42 +0200

    Lisätty Arvaa luku -peli C#-kielellä.

---
+++ DotNet/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ArvaaLukuPeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa peliin!");
+
+            Random random = new Random();
+            int oikea = random.Next(1, 21);
+            Console.WriteLine(oikea);
+
+            int arvauskertoja = 0;
+            while (arvauskertoja < 3)
+            {
+                Console.WriteLine("Anna arvauksesi välillä 1-20:");
+                string syöte = Console.ReadLine();
+                int arvaus = int.Parse(syöte);
+
+                if (arvaus < oikea)
+                {
+                    Console.WriteLine("Oikea luku on suurempi!");
+                }
+                else if (arvaus > oikea)
+                {
+                    Console.WriteLine("Oikea luku on pienempi!");
+                }
+                else
+                {
+                    Console.WriteLine("Arvasit oikein, voitit pelin!");
+                    break;
+                }
+
+                arvauskertoja++;
+            }
+
+            Console.WriteLine("Peli on päättynyt.");
+        }
+    }
+}
