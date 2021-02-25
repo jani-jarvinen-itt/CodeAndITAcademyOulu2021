0d1c76ecd11c0ac6236c07f6f42eaae05e3d0373
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-25 09:29:43 +0200

    Lisätty esimerkki C#:lla tiedoston lukujen summan laskennasta.

---
+++ DotNet/TiedostonLukujenSumma/TiedostonLukujenSumma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace TiedostonLukujenSumma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string tiedosto = @"C:\Academy\Oulu\CodeAndIT2021\Git\CodeAndITAcademyOulu2021\DotNet\TiedostonLukujenSumma\Luvut.txt";
+
+            float summa = 0.0f;
+            CultureInfo enUS = new CultureInfo("en-US");
+            string[] rivit = File.ReadAllLines(tiedosto);
+            foreach (string rivi in rivit)
+            {
+                try
+                {
+                    float luku = float.Parse(rivi, enUS);
+                    summa += luku;
+                }
+                catch
+                {
+                    // ei tehdä mitään
+                }
+            }
+
+            Console.WriteLine("Lukujen summa on: " + summa);
+        }
+    }
+}
