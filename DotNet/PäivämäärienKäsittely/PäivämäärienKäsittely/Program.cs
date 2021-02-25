db04bda04782f150daa8ca308aac37d9d7593462
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-25 11:01:46 +0200

    Lisätty esimerkki päivämäärn käsittelystä eri kulttuureilla.

--- DotNet/PäivämäärienKäsittely/PäivämäärienKäsittely/Program.cs
+++ DotNet/PäivämäärienKäsittely/PäivämäärienKäsittely/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PäivämäärienKäsittely
 {
@@ -11,13 +12,31 @@ namespace PäivämäärienKäsittely
             Console.WriteLine("Anna päivämäärä 2:");
             string syöte2 = Console.ReadLine();
 
-            DateTime pvm1 = DateTime.Parse(syöte1);
-            DateTime pvm2 = DateTime.Parse(syöte2);
+            DateTime pvm1 = MuunnaPäivämäärä(syöte1);
+            DateTime pvm2 = MuunnaPäivämäärä(syöte2);
 
             TimeSpan erotus = pvm2 - pvm1;
             Console.WriteLine($"Erotus: {erotus.TotalDays/7} viikkoa.");
             Console.WriteLine($"Erotus: {erotus.TotalDays} päivää.");
             Console.WriteLine($"Erotus: {erotus.TotalHours} tuntia.");
+        }
+
+        static DateTime MuunnaPäivämäärä(string syöte)
+        {
+            DateTime tulos;
+
+            CultureInfo fiFi = new CultureInfo("fi-FI");
+            CultureInfo enUS = new CultureInfo("en-US");
+            try
+            {
+                tulos = DateTime.Parse(syöte, fiFi);
+            }
+            catch
+            {
+                tulos = DateTime.Parse(syöte, enUS);
+            }
+
+            return tulos;
         }
     }
 }
