a2a7f5381d9aeedea34716c26d6b361179702934
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-03 13:38:17 +0200

    Lisätty Entity-mallin luomat tiedostot.

---
+++ DotNet/NorthwindEFDemo/NorthwindEFDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NorthwindEFDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
