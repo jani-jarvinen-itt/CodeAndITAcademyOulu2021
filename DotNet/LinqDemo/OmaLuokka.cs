babf7da5b4a4d21b4c5ce9ea93f0a439f3f6cf19
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-03 10:20:51 +0200

    Lisätty data-luokka LINQ-demoon.

---
+++ DotNet/LinqDemo/OmaLuokka.cs
@@ -0,0 +1,9 @@
+namespace LinqDemo
+{
+    public class OmaLuokka
+    {
+        public int Luku { get; set; }
+        public string Nimi { get; set; }
+        public string Kaupunki { get; set; }
+    }
+}
