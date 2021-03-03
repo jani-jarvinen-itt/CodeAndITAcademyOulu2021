8b29793ecd86f00cbec50515b843c3e460357266
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-03 09:29:19 +0200

    Lisätty perinteisen koodauksen esimerkki numerotaulukon käsittelystä.

--- DotNet/LinqDemo/Program.cs
+++ DotNet/LinqDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LinqDemo
 {
@@ -7,6 +8,21 @@ namespace LinqDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] luvut = { 5, 3, 10, 8, 4, 1, 9, 7, 2, 11 };
+            
+            // tehtävä: etsi viittä suuremmat luvut ja tulosta ne numerojärjestyksessä
+            List<int> tulokset = new List<int>();
+            foreach (int luku in luvut) {
+                if (luku > 5) {
+                    tulokset.Add(luku);
+                }
+            }
+
+            tulokset.Sort();
+            foreach (int tulos in tulokset) {
+                Console.WriteLine(tulos);
+            }
         }
     }
 }
